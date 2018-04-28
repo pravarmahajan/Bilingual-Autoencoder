255d5424e1458efb9d3d14863bb940ee4d4a46de
Pravar Mahajan <pravar.d.mahajan@gmail.com>
2018-04-28 13:55:50 -0400

    add commandline arguments for easier hyperparameter tuning; check run.sub for usage

--- run.py
+++ run.py
@@ -8,17 +8,23 @@
 
 from correlational_net import *
 import pickle
+import argparse
 
-lamda = 4       # The lambda parameter explained in the paper
+argp = argparse.ArgumentParser()
+argp.add_argument('--lamda', type=int, default=4, action='store')
+argp.add_argument('--nhid', type=int, default=128, action='store')
+argp.add_argument('--nepochs', type=int, default=100, action='store')
+parser = argp.parse_args()
+
+lamda = parser.lamda       # The lambda parameter explained in the paper
 batch_size=256   # No. of examples per batch
-nhid = 128	# number of hidden neurons
+nhid = parser.nhid	# number of hidden neurons
 data_filename = "./en_iu.npz"
 data = load(data_filename)
 nvis = data.shape[1]
 fts1 = len(pickle.load(open('./data/eng_word2id.pkl', 'rb')))	# number of features in first view
-#fts1 = 91808 # number of features in first view
 fts2 = nvis-fts1 # number of features in second view
 assert(fts2==len(pickle.load(open('./data/inuk_word2id.pkl', 'rb'))))
-training_epochs = 20  # No. of training epochs
+training_epochs = parser.nepochs  # No. of training epochs
 
 corr_net(data, nvis=nvis,nhid=nhid,fts1=fts1,fts2=fts2, lamda = lamda, training_epochs=training_epochs,batch_size=batch_size)
